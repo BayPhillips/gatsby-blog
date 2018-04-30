aed0ae2c5a9263229b8538dd8dddf6aabf1839d9
Bay Phillips <bay.phillips@gmail.com>
2018-04-29 21:16:03 -0400

    local plugin working

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`
+    `gatsby-plugin-sharp`,
+    `gatsby-source-plated`
   ],
 }
